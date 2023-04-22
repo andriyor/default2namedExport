7ed0686a712867cba269784c6feed06b81da270e
Andrii Oriekhov <andriyorehov@gmail.com>
2023-04-22 22:18:14 +0300

    add example of export under different name

--- test/test-project/src/components/main/index.ts
+++ test/test-project/src/components/main/index.ts
@@ -1,3 +1,3 @@
 export { default } from './Button';
 export { default as Image } from './Image';
-export { default as VideoRenamed } from './Image';
+export { default as VideoRenamed } from './Video';
