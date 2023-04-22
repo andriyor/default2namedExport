314e5bfa662686ee101ff2d9ee1d4de2465cd6a1
Andrii Oriekhov <andriyorehov@gmail.com>
2023-04-22 22:16:26 +0300

    add example of export under different name

--- test/test-project/A-usage.ts
+++ test/test-project/A-usage.ts
@@ -1,9 +1,11 @@
 import ComponentNameRenamed from './B-method';
 import Button from './src/components/main';
 import { Image } from './src/components/main';
+import { VideoRenamed } from './src/components/main';
 import Input from './src/components/main/Input';
 
 console.log(ComponentNameRenamed());
 console.log(Button());
 console.log(Input());
 console.log(Image());
+console.log(VideoRenamed());
