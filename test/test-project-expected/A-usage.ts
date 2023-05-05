e5569ddae9f3259b75099090cb71394be681a987
Andrii Oriekhov <andriyorehov@gmail.com>
2023-05-05 19:50:06 +0300

    add mixed import test

--- test/test-project-expected/A-usage.ts
+++ test/test-project-expected/A-usage.ts
@@ -2,7 +2,7 @@ import { ComponentName } from './B-method';
 import { Button } from './src/components/main';
 import { Image } from './src/components/main';
 import { Video } from './src/components/main';
-import { Input, inputFormat } from './src/components/main/Input';
+import { inputFormat, Input } from './src/components/main/Input';
 
 console.log(ComponentName());
 console.log(Button());
